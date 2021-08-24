40b0a4fc4cc4ac1e559503cfb5bd997a5dda1be9
Taco <thisistacotaco@gmail.com>
2021-08-24 20:46:30 +0100

    first

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./nav.css";
+import { GiBeard } from "react-icons/gi";
+import { BsGrid } from "react-icons/bs";
+import { BsCollection } from "react-icons/bs";
+import { BsChevronDown } from "react-icons/bs";
+import { BsBook } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <div className="sidebar">
+      <div className="logo-details">
+        <i>
+          <GiBeard />
+        </i>
+        <span className="logo_name">Tom C</span>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <a href="#">
+            <i>
+              <BsGrid />
+            </i>
+            <span className="link_name">Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <div className="icon-link">
+            <a href="#">
+              <i>
+                <BsCollection />
+              </i>
+              <span className="link_name">Category</span>
+            </a>
+            <i>
+              <BsChevronDown />
+            </i>
+          </div>
+          <ul className="sub-menu">
+            <li>
+              <a href="#">Web Design</a>
+              <a href="#">Card Design</a>
+              <a href="#">Login Forms</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <div className="icon-link">
+            <a href="#">
+              <i>
+                <BsBook />
+              </i>
+              <span className="link_name">Posts</span>
+            </a>
+            <i>
+              <BsChevronDown />
+            </i>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
